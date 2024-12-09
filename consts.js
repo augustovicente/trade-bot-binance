4f8c686ddf47a38c201c7f1870d0b2b9e3536dca
Augusto Vicente <devguto@gmail.com>
2024-12-09 22:07:59 +0000

    first version

---
+++ consts.js
@@ -0,0 +1,4 @@
+import 'dotenv/config';
+
+export const API_KEY = process.env.API_KEY;
+export const API_SECRET = process.env.API_SECRET;
---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+const main = async () => {
+    // For all of the currencies check if there is an order opened
+        // If there is an order opened, check when was it opened
+            // If it was more than 30 minutes ago, check if it is the last price option
+                // If it is the last price option, cancel the order and open a new one
+};
+
+main();
